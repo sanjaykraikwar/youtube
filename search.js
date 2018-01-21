d46c1d70cb1a842a410bd73dd2aab2909bea14ad
sanjaykraikwar <raikwarsanjaykumar@gmail.com>
2018-01-21 12:22:55 +0530

    'modified'

--- search.js
+++ search.js
@@ -2,7 +2,7 @@
 
 // Search for a specified string.
 function search() {
-	
+
 	var keyword = document.getElementById('query').value;
 
 	var req = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyB7VkFIbTYbzvFfDV8A7eiMWdKQVSeQ3b0&type=video&part=snippet&maxResults=15&q=' + keyword;
@@ -15,35 +15,35 @@ function search() {
 		var maincontents = document.getElementById("maincontents");
 
 		maincontents.innerHTML = '';
-		
+
 		var slide = new Array(4);
-		var count=0;
+		var count = 0;
 		for (let i = 0; i < 4; i++) {
 			slide[i] = document.createElement('div');
 
 			slide[i].setAttribute('class', 'mySlides');
-			
+
 			var str1 = '<div class="row pagination">';
 
-			var str2='';
-			
-			for (let j = 0; j < 4; j++,count++) {
-			str2= str2+	'<div class="column"><img style="width:98%" id="img' + count + '"><div id="titlediv' + count + '" class="centered blue-grey"></div><div id="detailsdiv' + count + '"></div></div>';
+			var str2 = '';
+
+			for (let j = 0; j < 4; j++ , count++) {
+				str2 = str2 + '<div class="column"><img style="width:98%" id="img' + count + '"><div id="titlediv' + count + '" class="centered blue-grey"></div><div id="detailsdiv' + count + '"></div></div>';
 			}
-			
-			var k=i+1;
 
-			var str3='<div class="display-bottommiddle"><a href="#">&laquo;</a><a href="#">'+k+'</a><a href="#">&raquo;</a></div></div>';
+			var k = i + 1;
+
+			var str3 = '<div class="display-bottommiddle"></div></div>';
 
 
-			slide[i].innerHTML = str1+str2+str3;
+			slide[i].innerHTML = str1 + str2 + str3;
 
 			maincontents.appendChild(slide[i]);
 
 		}
 
-		
-		var videoids ='';
+
+		var videoids = '';
 
 		for (let i = 0; i < 15; i++) {
 
@@ -52,7 +52,7 @@ function search() {
 			var x = document.getElementById("img" + i);
 			x.setAttribute("src", data.items[i].snippet.thumbnails.high.url);
 			x.setAttribute("alt", "The Pulpit Rock");
-		
+
 			/* for setting image end*/
 
 			/* for setting title start*/
@@ -63,88 +63,101 @@ function search() {
 			titlediv.appendChild(para);
 			/* for setting title end*/
 
-
-
 			/* for setting details start*/
 
-			var description = document.createTextNode(data.items[i].snippet.description);
+			var description = document.createTextNode(data.items[i].snippet.description.substring(0, 100) + '...');
 			var author = document.createTextNode(data.items[i].snippet.channelTitle);
 			var published_date = document.createTextNode(data.items[i].snippet.publishedAt);
-			
-			if(videoids==''){
+
+			if (videoids == '') {
 				videoids = data.items[i].id.videoId;
 			}
-			else{
-			videoids = videoids+','+data.items[i].id.videoId;
+			else {
+				videoids = videoids + ',' + data.items[i].id.videoId;
 			}
 
 
 			var detailsdiv = document.getElementById("detailsdiv" + i);
 			//detailsdiv.setAttribute('class', 'details');
-			
+
 			var descdiv = document.createElement('div');
 
 			descdiv.setAttribute('class', 'description');
-			
-			
-			var para1 = document.createElement("p");
-			
-			var para2 = document.createElement("p");
-			
-			var para3 = document.createElement("p");
-			
-			para3.setAttribute('id', 'video'+i);
-			
-			para1.innerHTML ='<i class="fa fa-male fa-2x icon" aria-hidden="true"></i><span class="details">'+ data.items[i].snippet.channelTitle +'</span>' ;
-			
-			para2.innerHTML = '<i class="fa fa-calendar fa-2x icon" aria-hidden="true"></i><span class="details">' + data.items[i].snippet.publishedAt +'</span>' ;
-				
-			detailsdiv.appendChild(para1);
-			
-			detailsdiv.appendChild(para2);
-			
-			detailsdiv.appendChild(para3);
-			
-			
-			descdiv.appendChild(description);
-			
-			detailsdiv.appendChild(descdiv);
-			
-			showDivs(1);
 
+
+			var para1 = document.createElement("p");
+
+			var para2 = document.createElement("p");
+
+			var para3 = document.createElement("p");
+
+			para3.setAttribute('id', 'video' + i);
+
+			para1.innerHTML = '<i class="fa fa-male fa-2x icon" aria-hidden="true"></i><span class="details">' + data.items[i].snippet.channelTitle + '</span>';
+
+			para2.innerHTML = '<i class="fa fa-calendar fa-2x icon" aria-hidden="true"></i><span class="details">' + data.items[i].snippet.publishedAt + '</span>';
+
+			detailsdiv.appendChild(para1);
+
+			detailsdiv.appendChild(para2);
+
+			detailsdiv.appendChild(para3);
+
+
+			descdiv.appendChild(description);
+
+			detailsdiv.appendChild(descdiv);
 			/* for setting details end*/
 
-		}
-		
-		var request = 'https://www.googleapis.com/youtube/v3/videos?key=AIzaSyB7VkFIbTYbzvFfDV8A7eiMWdKQVSeQ3b0&id='+videoids+'&part=snippet,statistics';
-		
-		console.log(request);
-				
-		
-	 fetch(request).then(function (response) {
+			/* paging div*/
+			var pagediv = document.getElementById("pagediv");
 
-		return response.json();
-	}).then(function (data) {
-		
-		console.log("response");
+			pagediv.innerHTML = '<a onclick="showPage(1)">1</a><a onclick="showPage(2)" >2</a><a onclick="showPage(3)">3</a><a onclick="showPage(4)">4</a>';
+
+
+			showDivs(1);
+
+
+
+
+
+		}
+
+		var request = 'https://www.googleapis.com/youtube/v3/videos?key=AIzaSyB7VkFIbTYbzvFfDV8A7eiMWdKQVSeQ3b0&id=' + videoids + '&part=snippet,statistics';
+
+		console.log(request);
+
+
+		fetch(request).then(function (response) {
+
+			return response.json();
+		}).then(function (data) {
+
+			console.log("response");
 			for (let i = 0; i < 15; i++) {
-				
-				document.getElementById('video'+i).innerHTML = '<i class="fa fa-eye fa-2x icon" aria-hidden="true"></i><span class="details">'+data.items[i].statistics.viewCount+'</span>' ;
-		
+
+				document.getElementById('video' + i).innerHTML = '<i class="fa fa-eye fa-2x icon" aria-hidden="true"></i><span class="details">' + data.items[i].statistics.viewCount + '</span>';
+
 			}
 
-	}).catch(function () {
-		console.log("error");
-	});
+		}).catch(function () {
+			console.log("error");
+		});
 
 	}).catch(function () {
 		console.log("error");
 	});
-	
-	
-	
 
 
+
+
+
+}
+
+
+function showPage(page) {
+	slideIndex = page;
+	showDivs(page);
 }
 
 var slideIndex = 1;
@@ -155,18 +168,14 @@ function plusDivs(n) {
 function showDivs(n) {
 	var i;
 	var x = document.getElementsByClassName("mySlides");
-	document.getElementById("next").disabled = false; 
-	document.getElementById("previous").disabled = false;
-	if (n > x.length-1) { 
-	document.getElementById("next").disabled = true; 
-	document.getElementById("previous").disabled = false;
-	}
-	if (n < 2) {
-			document.getElementById("next").disabled = false; 
-	document.getElementById("previous").disabled = true;
-		}
+	var an = document.getElementsByTagName("a");
+
 	for (i = 0; i < x.length; i++) {
 		x[i].style.display = "none";
 	}
+	for (j = 0; j < an.length; j++) {
+		an[j].className = "";
+	}
 	x[slideIndex - 1].style.display = "block";
+	an[n - 1].className = "active"
 }
